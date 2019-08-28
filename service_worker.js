78d0430179cdd69e3b762637a95090e8fa9eca67
maru_cc <tomoyuki.maruta@gmail.com>
2019-08-28 10:54:20 +0900

    pwa化

---
+++ service_worker.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'bike_frame_calc';
+var urlsToCache = [
+    '/bike_frame_calc/',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
