94e859caf46996a20722b761d6addd47933684ae
maru_cc <tomoyuki.maruta@gmail.com>
2019-08-28 16:32:11 +0900

    modify service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,24 +1,26 @@
-var CACHE_NAME = 'bike_frame_calc';
+var CACHE_NAME = 'bike_frame_calc_2019082801';
 var urlsToCache = [
     '/bike_frame_calc/',
+    '/bike_frame_calc/lodash.min.js',
+    '/bike_frame_calc/vue.min.js',
 ];
 
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response ? response : fetch(event.request);
+      })
+  );
 });
